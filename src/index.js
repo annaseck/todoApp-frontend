f370833a89c3ad55dc97ef2e33802d0d01321b5a
annaseck <annaseck520@gmail.com>
2024-12-02 12:35:43 +0000

    vercel

--- src/index.js
+++ src/index.js
@@ -2,25 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { UserContextProvider } from './context/UserContext.jsx';
 import { TodoContextProvider } from './context/TodoContext.jsx';
-//import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/*", // Ensure all routes are matched
+      element: <App />,
+    },
+  ],
+  {
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    },
+  }
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <UserContextProvider>
       <TodoContextProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <RouterProvider router={router} />
       </TodoContextProvider>
     </UserContextProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
